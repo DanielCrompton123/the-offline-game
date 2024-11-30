e579da0b0bdf46bf101adac3685bd3247926e3ad
Daniel Crompton <danielcrompton5@gmail.com>
2024-11-30 21:29:31 +0000

    Initial Commit

---
+++ The Offline Game/The_Offline_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Offline_GameApp.swift
+//  The Offline Game
+//
+//  Created by Daniel Crompton on 11/30/24.
+//
+
+import SwiftUI
+
+@main
+struct The_Offline_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
