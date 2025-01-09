5d13e4a660e66a66c4fbabf0759d8f99dac596ab
Daniel Crompton <danielcrompton5@gmail.com>
2025-01-09 21:05:44 +0000

    Create the congrats view and add other assets

--- The Offline Game/View/Main/CongratulatoryView.swift
+++ The Offline Game/View/Main/CongratulatoryView.swift
@@ -8,11 +8,75 @@
 import SwiftUI
 
 struct CongratulatoryView: View {
+    
+    @Environment(OfflineViewModel.self) private var offlineViewModel
+    
     var body: some View {
-        Text("Congratulations!!!")
+        ZStack {
+            // GREEN BG
+            Color.green
+                .ignoresSafeArea()
+                .overlay {
+                    
+                    // OFFLINE IMAGE
+                    Image(.wifiBrushstrokeSlash)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(height: 600)
+                        .offset(x: 100, y: -100)
+                        .foregroundStyle(.white)
+                        .opacity(0.15)
+                    
+                }
+            
+            
+            // CONTENT
+            content
+        }
+    }
+    
+    
+    @ViewBuilder
+    private var content: some View {
+        VStack(spacing: 30) {
+            Spacer()
+            
+            Image(systemName: "checkmark")
+                .font(.system(size: 130, weight: .black))
+                .foregroundStyle(.green.gradient)
+                .shadow(color: .black.opacity(0.2), radius: 10, x: 10, y: 10)
+            
+            Text("You did it!")
+                .font(.display108)
+            
+            let formattedDur = DurationDisplayHelper.formatDuration(offlineViewModel.durationSeconds)
+            Text("You successfully spent \(formattedDur) offline!")
+                .opacity(0.75)
+                .font(.display40)
+            
+            Spacer()
+            
+            let congratulatoryImage = Image(.offlineCard)
+            ShareLink(
+                item: congratulatoryImage,
+                subject: Text("I just spent \(formattedDur) offline on the offline game!"),
+                message: Text("Do you think you could beat me? Why not give it a go!"),
+                preview: SharePreview("My \(formattedDur) offline!",
+                                      image: congratulatoryImage)
+            ) {
+                Label("Share my sucess!", systemImage: "medal.star.fill")
+            }
+            .buttonStyle(FilledRedButtonStyle())
+            
+            Spacer()
+        }
+        .foregroundStyle(.white)
+        .multilineTextAlignment(.center)
+        .padding(.horizontal, 40)
     }
 }
 
 #Preview {
     CongratulatoryView()
+        .environment(OfflineViewModel())
 }
