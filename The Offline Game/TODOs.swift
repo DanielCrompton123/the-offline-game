e90cca3ec06e5aa2678966929b48ed9257e46f54
Daniel Crompton <danielcrompton5@gmail.com>
2025-01-15 22:26:51 +0000

    Add TODOs as warnings

---
+++ The Offline Game/TODOs.swift
@@ -0,0 +1,19 @@
+//
+//  TODOs.swift
+//  The Offline Game
+//
+//  Created by Daniel Crompton on 1/15/25.
+//
+
+import Foundation
+
+#warning("Add an offline leaderboard, and game center objectives")
+#warning("Use the Duration API to format minutes/seconds")
+#warning("Fix the lock screen live activity")
+#warning("Create a better offline time gauge")
+#warning("Create a better offline duration slider")
+#warning("Add haptic feedback")
+#warning("Add “Share via instagram” button to congrats view")
+#warning("Add a widget to the home screen to go offline")
+#warning("Add attributions for fonts and lottie animations")
+#warning("Add common offline durations to picker")
