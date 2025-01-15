74b8b020b1e5595c0b8dd79e2a34aedcb07e73ed
Daniel Crompton <danielcrompton5@gmail.com>
2025-01-15 11:59:30 +0000

    Make the congrats view text display the elapsed time instead of the total time. Therefore it outputs the correct time when the user quits early.

--- The Offline Game/View/Results - Success or failure/CongratulatoryView.swift
+++ The Offline Game/View/Results - Success or failure/CongratulatoryView.swift
@@ -48,27 +48,31 @@ struct CongratulatoryView: View {
             Text("You did it!")
                 .font(.display108)
             
-            let formattedDur = DurationDisplayHelper.formatDuration(offlineViewModel.durationSeconds)
-            Text("You successfully spent \(formattedDur) offline!")
-                .opacity(0.75)
-                .font(.display40)
-            
-            Spacer()
-            
-            let congratulatoryImage = Image(.offlineCard)
-            ShareLink(
-                item: congratulatoryImage,
-                subject: Text("I just spent \(formattedDur) offline on the offline game!"),
-                message: Text("Do you think you could beat me? Why not give it a go!"),
-                preview: SharePreview("My \(formattedDur) offline!",
-                                      image: congratulatoryImage)
-            ) {
-                Label("Share success", systemImage: "medal.star.fill")
-                    .foregroundStyle(.green)
+            if let elapsedTime = offlineViewModel.elapsedTime {
+                let formattedDur = DurationDisplayHelper.formatDuration(elapsedTime)
+                
+                Text("You successfully spent \(formattedDur) offline!")
+                    .opacity(0.75)
+                    .font(.display40)
+                
+                Spacer()
+                
+                let congratulatoryImage = Image(.offlineCard)
+                ShareLink(
+                    item: congratulatoryImage,
+                    subject: Text("I just spent \(formattedDur) offline on the offline game!"),
+                    message: Text("Do you think you could beat me? Why not give it a go!"),
+                    preview: SharePreview("My \(formattedDur) offline!",
+                                          image: congratulatoryImage)
+                ) {
+                    Label("Share success", systemImage: "medal.star.fill")
+                        .foregroundStyle(.green)
+                }
+                .buttonStyle(FilledRedButtonStyle())
+                .tint(.white)
+                .padding(.horizontal, -40) // opposite of the padding added by the stack
+                
             }
-            .buttonStyle(FilledRedButtonStyle())
-            .tint(.white)
-            .padding(.horizontal, -40) // opposite of the padding added by the stack
         }
         .foregroundStyle(.white)
         .multilineTextAlignment(.center)
