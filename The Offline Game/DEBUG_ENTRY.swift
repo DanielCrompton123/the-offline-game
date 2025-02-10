48b4ffe18b3a0cd595f6b61c558e78e9895766e4
Daniel Crompton <danielcrompton5@gmail.com>
2025-02-10 15:47:21 +0000

    Complete the Horizontal ruler slider

--- The Offline Game/DEBUG_ENTRY.swift
+++ The Offline Game/DEBUG_ENTRY.swift
@@ -17,15 +17,6 @@ struct DEBUG: View {
             
             Circle()
                 .frame(width: 20, height: 20)
-            
-            
-            
-            SteppedSlider(value: $value, properties: [
-                .init(range: 0.0...25.0, spacing: 20, step: 1, color: .red),
-                .init(range: 25.0...50.0, spacing: 30, step: 5, color: .blue),
-                .init(range: 50.0...100.0, spacing: 50, step: 10, color: .pink)
-            ])
-            .frame(height: 50)
         }
 
     }
